236c1a85f425d95db73607e85b575ec7354fc721
rrgsoft2m <rrgsoft2m@gmail.com>
2026-02-20 20:52:54 +0500

    Update API URL in next.config.js

--- admin/next.config.js
+++ admin/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: { remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }] },
-    env: { NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000' },
+    env: { NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://bakery-delivery-backendbakery-delivery.onrender.com' },
 };
 module.exports = nextConfig;
